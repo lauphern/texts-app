ca47085d0293430e65290bc43a45ceb39ed4e5d2
lauphern <lau.p.hern@hotmail.com>
2020-06-19 13:38:26 +0200

    added environment variables

---
+++ frontity.settings.js
@@ -0,0 +1,63 @@
+import { config } from "dotenv";
+
+// Launch dot-env.
+config();
+
+const settings = {
+  "name": "texts-app",
+  "state": {
+    "frontity": {
+      "url": "https://test.frontity.org",
+      "title": "Test Frontity Blog",
+      "description": "WordPress installation for Frontity development"
+    }
+  },
+  "packages": [
+    {
+      "name": "@frontity/mars-theme",
+      "state": {
+        "theme": {
+          "menu": [
+            [
+              "Home",
+              "/"
+            ],
+            [
+              "Nature",
+              "/category/nature/"
+            ],
+            [
+              "Travel",
+              "/category/travel/"
+            ],
+            [
+              "Japan",
+              "/tag/japan/"
+            ],
+            [
+              "About Us",
+              "/about-us/"
+            ]
+          ],
+          "featured": {
+            "showOnList": false,
+            "showOnPost": false
+          }
+        }
+      }
+    },
+    {
+      "name": "@frontity/wp-source",
+      "state": {
+        "source": {
+          // "api": "https://test.frontity.org/wp-json"
+          "api": process.env.PACKAGES_STATE_API
+        }
+      }
+    },
+    "@frontity/tiny-router",
+    "@frontity/html2react"
+  ]
+};
+
+export default settings;
